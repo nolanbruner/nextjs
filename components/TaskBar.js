8fb9df8d9f3297fc7ac88e270d39a9cafe6a5f08
nolanbruner <nolansname@aol.com>
2023-01-11 06:54:44 -0500

    added chatGPT

---
+++ components/TaskBar.js
@@ -0,0 +1,19 @@
+import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar'
+import Link from "next/link"
+
+export default function TaskBar() {
+ return(
+<>      
+  <AppBar color="primary" >
+            <Toolbar variant='dense' >
+                <Box  sx={{ pr: 6 }}><Link href="/" color="inherit" >Home</Link></Box>
+                <Box  sx={{ pr: 6 }}><Link href="/about" color='inherit'>Calendar</Link></Box>
+            </Toolbar>
+
+        </AppBar>
+
+    </>
+)}
