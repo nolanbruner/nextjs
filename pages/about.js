ba179a69d3677a356d5764bec7a96911376571c1
Jackson Blankenship <jacksblan@gmail.com>
2023-01-25 23:19:06 -0600

    format javascript

--- pages/about.js
+++ pages/about.js
@@ -1,63 +1,58 @@
-import styles from '../styles/Home.module.css'
-import Link from "next/link"
-import dayjs from 'dayjs';
-import React, {useState } from 'react'
+import styles from '../styles/Home.module.css';
+import React from 'react';
 import TaskBar from '../components/TaskBar';
 import TextField from '@mui/material/TextField';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-
-import  Grid from '@mui/material/Grid';
-import Paper from "@mui/material/Paper"
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-// import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 export default function about() {
-    // const [Value, setValue] = useState({})
-    return (
-        <>
-            <TaskBar />
-
-            <main className={styles.main}>
-                <Grid container spacing={2}>
-                    <Grid item xs={4}>
-                        <Item>
-                            <LocalizationProvider sx={{ color: "black" }} dateAdapter={AdapterDayjs} >
-                                <StaticDatePicker
-                                    // orientation="landscape"
-
-                                    openTo="day"
-                                    // value={Value}
-                                    // onChange={(newValue) => {
-                                    //     setValue(newValue);
-                                    // }}
-
-                                    renderInput={(params) => <TextField {...params} sx={{ color: "black" }} />}
-                                />
-                            </LocalizationProvider>
-                        </Item>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Item>xs=4</Item>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Item>xs=4</Item>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Item>xs=8</Item>
-                    </Grid>
-                </Grid>
-            </main>
-        </>
-    )
-} 
+  return (
+    <>
+      <TaskBar />
+      <main className={styles.main}>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <Item>
+              <LocalizationProvider
+                sx={{ color: 'black' }}
+                dateAdapter={AdapterDayjs}>
+                <StaticDatePicker
+                  // orientation="landscape"
+                  openTo="day"
+                  // value={Value}
+                  // onChange={(newValue) => {
+                  //     setValue(newValue);
+                  // }}
+                  renderInput={params => (
+                    <TextField {...params} sx={{ color: 'black' }} />
+                  )}
+                />
+              </LocalizationProvider>
+            </Item>
+          </Grid>
+          <Grid item xs={8}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+      </main>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,34 @@
-import Head from "next/head";
-import { useState } from "react";
-import TaskBar from "../components/TaskBar";
-import Box from "@mui/material/Box"
-import styles from '../styles/Home.module.css'
-import Form from "@mui/material/FormGroup"
-import { Input } from "@mui/material";
+import { useState } from 'react';
+import TaskBar from '../components/TaskBar';
+import Box from '@mui/material/Box';
+import styles from '../styles/Home.module.css';
+import { Input } from '@mui/material';
+
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [animalInput, setAnimalInput] = useState('');
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
     event.preventDefault();
     try {
-      const response = await fetch("/api/hello", {
-        method: "POST",
+      const response = await fetch('/api/hello', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({ animal: animalInput }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
       setResult(data.result);
-      setAnimalInput("");
+      setAnimalInput('');
     } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -39,23 +41,21 @@ export default function Home() {
       <TaskBar />
       <main className={styles.main}>
         <Box>
-        <h3 className={styles.className}>Ask Me Anything</h3>
-        <form onSubmit={onSubmit}>
-          <Input
-            sx={{width:"40vw"}}
-            type="text"
-            name="animal"
-            placeholder="Enter an Question"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <Input type="submit" value="Generate Answer" />
-        </form>
-        <Box sx={{width:"60vw"}}>{result}</Box>
+          <h3 className={styles.className}>Ask Me Anything</h3>
+          <form onSubmit={onSubmit}>
+            <Input
+              sx={{ width: '40vw' }}
+              type="text"
+              name="animal"
+              placeholder="Enter an Question"
+              value={animalInput}
+              onChange={e => setAnimalInput(e.target.value)}
+            />
+            <Input type="submit" value="Generate Answer" />
+          </form>
+          <Box sx={{ width: '60vw' }}>{result}</Box>
         </Box>
-        
       </main>
-      
     </>
   );
 }
