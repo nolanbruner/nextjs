3b0e4ffe30bb1205085047209aae316b7cf4731f
nolanbruner <nolansname@aol.com>
2023-02-27 18:45:05 -0500

    added tradingView

--- pages/finance.js
+++ pages/finance.js
@@ -14,7 +14,8 @@ import { getNews } from './api/finance';
 import { DropDownOption, DropDownButton } from "../components/buttons";
 import { Line, Bar } from 'react-chartjs-2';
 import * as Highcharts from 'highcharts';
-
+import Box from '@mui/material/Box';
+import TradingViewWidget from '../components/candle';
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
@@ -51,45 +52,22 @@ export default function about() {
   useEffect(() => {
     handle();
   }, [])
-  const [timeFrame, setTimeFrame] = useState("");
   // if (window.screen.availWidth >= 840) {
-    
-    return (
-      <>
-        <TaskBar />
-        <main className={styles.main}>
-          <button onClick={() => { setTimeFrame("Day") }} select="true">Day</button>
-          <button onClick={() => { setTimeFrame("Month") }}>Month</button>
-          <button onClick={() => { setTimeFrame("Year") }}>Year</button>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <Item>
-                <Bar options={barChart.options} data={barChart.data} />
-                {/* <LocalizationProvider
-                sx={{ color: 'black' }}
-                dateAdapter={AdapterDayjs}>
-                <StaticDatePicker
-                  // orientation="landscape"
-                  openTo="day"
-                  // value={Value}
-                  // onChange={(newValue) => {
-                  //     setValue(newValue);
-                  // }}
-                  renderInput={params => (
-                    <TextField {...params} sx={{ color: 'black' }} />
-                  )}
-                />
-              </LocalizationProvider> */}
-              </Item>
-            </Grid>
-            <Grid item xs={6}>
-              <Line options={lineChart.options(timeFrame)} data={lineChart.data(timeFrame)} />
-            </Grid>
-          </Grid>
-          
-        </main>
-      </>
-    );
+
+  return (
+    <>
+      <TaskBar />
+      <main className="container">
+        <Box pt="18px" height="90vh" width="90vw">
+
+          <TradingViewWidget />
+
+
+
+        </Box>
+      </main>
+    </>
+  );
   // }
 
 }
